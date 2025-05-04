9377d4c0e7a26a2babeca92f79647806d86d8120
MrAust1n <craig@mraustin.co.uk>
2025-05-04 18:41:45 +0100

    UPDATE: Oders/ Models
    I have updated orders/models.py and added some standard settings for now

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
+# orders/models.py
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from items.models import Item
+
+
+class Order(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    stripe_payment_intent = models.CharField(max_length=255)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.item.title}"
