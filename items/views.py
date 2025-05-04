c6636d7bbb303bea3c4b2b5ed8165909a9e1372b
MrAust1n <craig@mraustin.co.uk>
2025-05-04 21:40:19 +0100

    FEAT: Image upload
    1st section of being able to upload the image.

--- items/views.py
+++ items/views.py
@@ -1,6 +1,19 @@
 # items/Views.py
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import ItemForm
 
 
-def home(request):
-    return render(request, "items/home.html")
+@login_required
+def add_item(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.seller = request.user
+            item.save()
+            return redirect("home")  # You’ll define this URL name
+    else:
+        form = ItemForm()
+    return render(request, "items/add_item.html", {"form": form})
