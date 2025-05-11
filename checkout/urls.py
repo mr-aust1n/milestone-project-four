7cbc4fe81541e14cd83a0cd1d6b9db6f8a1d8f3f
MrAust1n <craig@mraustin.co.uk>
2025-05-11 21:19:06 +0100

    FEAT: Checkout urls
    Added Checkout URLs and the code for the checkout

---
+++ checkout/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("buy/<int:item_id>/", views.create_checkout_session, name="buy_item"),
+    path("success/<int:item_id>/", views.payment_success, name="payment_success"),
+]
