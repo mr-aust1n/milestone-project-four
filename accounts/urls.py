31a2e8c4dbf07a9584ffcf4eca12acb3a7ef33e9
MrAust1n <craig@mraustin.co.uk>
2025-06-01 21:42:11 +0100

    STYLE: Added styles to the password reset
    I have used the same CSS file for all the password reset sections

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,35 +1,48 @@
 # accounts/urls.py
+
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    # Registration
     path("signup/", views.register, name="signup"),
     path("activate/<uidb64>/<token>/", views.activate, name="activate"),
+    # Login & Logout
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # ✅ Password reset URLs
+    # Password Reset
     path(
-        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
+        ),
+        name="password_reset",
     ),
     path(
-        "password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
         name="password_reset_confirm",
     ),
     path(
         "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
         name="password_reset_complete",
     ),
 ]
