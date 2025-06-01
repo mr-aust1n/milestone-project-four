dc0a27d285eebf127a109aac9a53e96525e39d7d
MrAust1n <craig@mraustin.co.uk>
2025-06-01 20:46:12 +0100

    FEAT: Password Reset
    Step 1 of a password reset.  Created Password_reset_form.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,23 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # ✅ Password reset URLs
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
