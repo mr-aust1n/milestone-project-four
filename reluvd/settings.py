a920ae3e02c6dde0550d91794b903bc408fdf001
MrAust1n <craig@mraustin.co.uk>
2025-06-11 20:08:07 +0100

    Trigger Heroku rebuild after settings.py fix

--- reluvd/settings.py
+++ reluvd/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
