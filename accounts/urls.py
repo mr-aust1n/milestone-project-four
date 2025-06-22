e64f4372b16138fd1f85f01ab50ddd9c3fe13766
MrAust1n <craig@mraustin.co.uk>
2025-06-22 12:43:31 +0100

    FIX: Logged out
    Changed the code on base.html .  removed <a href="{% url 'logout' %}">Logout</a>  as there was 2 log outs

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # Password Reset
     path(
         "password-reset/",
--- reluvd/urls.py
+++ reluvd/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
     path("accounts/", include("accounts.urls")),
     path("checkout/", include("checkout.urls")),
     path("orders/", include("orders.urls")),
